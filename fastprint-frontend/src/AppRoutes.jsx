8246a1bc618a66777a272f8e9567204240cccce1
AyanAhmed30 <hafizayanahmed9@gmail.com>
2025-08-17 02:10:04 +0500

    Added/Updated AppRoutes.jsx for CI/CD

--- fastprint-frontend/src/AppRoutes.jsx
+++ fastprint-frontend/src/AppRoutes.jsx
@@ -8,6 +8,7 @@ import Shop from './pages/Shop'
 import CoverExpert from './pages/CoverExpert';
 import Payment from './pages/Payment';
 import Products from './pages/Products';
+import Services from './pages/Services';
 // Products
 import PrintBook from './pages/products/PrintBook';
 import ComicBook from './pages/products/ComicBook';
@@ -138,6 +139,7 @@ const AppRoutes = () => {
       <Route path="/resources/order-lookup" element={<OrderLookup />} />
       <Route path="/resources/plan-project" element={<PlanProject />} />
       <Route path="/book-pricing" element={<BookPricing />} />
+      <Route path="/services" element={<Services />} />
 
       {/* Auth & Verification Routes */}
       <Route path="/reset-password" element={<ResetPassword />} />
